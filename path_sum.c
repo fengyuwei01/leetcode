3273701ddb2970686afdf888a547c56cf8d76ada
fengzi <fengyuwei01@gmail.com>
2017-08-09 23:52:02 +0800

    update

---
+++ path_sum.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool is_path_sum(struct TreeNode* root, int sum) {
+    if (root->left == NULL && root->right == NULL) {
+        return sum == root->val;
+    } else {
+        bool bol = false;
+        if (root->left) {
+            bol = bol || is_path_sum(root->left,sum - root->val);
+        }
+        if (root->right) {
+            bol = bol || is_path_sum(root->right,sum - root->val);
+        }
+        
+        return bol;
+    }
+}
+
+bool hasPathSum(struct TreeNode* root, int sum) {
+    if (root == NULL) {
+        return false;
+    }
+    return is_path_sum(root,sum);
+}
