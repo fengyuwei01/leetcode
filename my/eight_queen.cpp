8f2e8956766fcae307f2120bae6a133e7b9688d3
fengyuwei <fengyuwei@baidu.com>
2017-08-17 14:13:47 +0800

    update

--- my/eight_queen.cpp
+++ my/eight_queen.cpp
@@ -8,51 +8,52 @@ using namespace std;
 // 8X8格的国际象棋上摆放八个皇后，使其不能互相攻击，
 // 即任意两个皇后都不能处于同一行、同一列或同一斜线上。
 #define N 8
-int chess[N][N] = {0};
+int g_chess[N][N];
 
 void chess_visitor() {
     int i=0,j=0;
     for(;i<N;i++) {
         for(j=0;j<N;j++) {
-            cout << " " << chess[i][j] << " ";
+            cout << " " << g_chess[i][j] << " ";
         }
         cout << endl;
     }
 }
 
 // 约束条件
-bool queue_constrain(int x,int y) {
+bool queen_constrain(int x,int y) {
     int i,j;
+    if (x == 0)
+    {
+        return true;
+    }
 
     // 同列
     for (i=0;i<x;i++) {
-        if (chess[i][y] > 0) {
+        if (g_chess[i][y] == 1) {
             return false;
         }
     }
     
     // 对角线
     for ( i = x-1,j = y-1; i>=0 && j>=0; i--,j--) {
-        if (chess[i][j] > 0) {
+        if (g_chess[i][j] == 1) {
             return false;
         }
     }
 
-    for ( i=x-1,j=y+1;i>=0 && y<N; i--,j++) {
-        if (chess[i][j] > 0) {
+    for ( i=x-1,j=y+1;i>=0 && j<N; i--,j++) {
+        if (g_chess[i][j] == 1) {
             return false;
         }
     }
 
-    if (x==0) {
-        printf("x,y [%d,%d]\n",x,y);
-    }
     return true;
 }
 
 int g_count = 0;
-void _eight_queue(int level) {
-    if (level >= N) {
+void _eight_queen(int level) {
+    if (level >= 8) {
         cout << "ok" << ":" << ++g_count << endl;
         // if (g_count > 3) exit(0);
         chess_visitor();
@@ -60,21 +61,19 @@ void _eight_queue(int level) {
     } else {
         int i;
         for (i=0;i<N;i++) {
-            if ( level == 0 && i > 1) {
-                printf("level:%d i:%d\n",level,i);
+            if (queen_constrain(level,i)) {
+                g_chess[level][i] = 1;
+                _eight_queen(level+1);
+                g_chess[level][i] = 0;
             }
-            chess[level][i] = 1;
-            if (queue_constrain(level,i)) {
-                _eight_queue(level+1);
-            }
-            chess[level][i] = 0;
         }
     }
 }
 
 void eight_queue() {
     // 初始化
-    _eight_queue(0);
+    memset(g_chess,0,sizeof(g_chess));
+    _eight_queen(0);
 }
 
 
